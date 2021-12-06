9ffe53f53314cd7825627a8ce20fd5775498d141
Nick Scrimale <bscrinale@gmail.com>
2021-12-05 18:48:29 -0600

    edit

---
+++ scripts/size.js
@@ -0,0 +1,30 @@
+import { getSizes, setSize } from "./database.js"
+
+const sizes = getSizes()
+
+
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.name === "size") {
+            setSize(parseInt(event.target.value))
+        }
+    }
+)
+
+
+export const ConeSizes = () => {
+    let html = "<ul>"
+
+    
+    const listItemsArray = sizes.map(size => {
+        return `<li>
+            <input type="radio" name="size" value="${size.id}" /> ${size.size}
+        </li>`
+    })
+
+    html += listItemsArray.join("")
+    html += "</ul>"
+
+    return html
+}
